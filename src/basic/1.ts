6d4b59f7b1fdfb849c91463eae7195e175a60970
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-04 19:02:43 +0200

    Basic tasks 1

---
+++ src/basic/1.ts
@@ -0,0 +1,13 @@
+let age: number = 50;
+let username: string = "Max";
+let toggle: boolean = true;
+let empty: null = null;
+let callback = (a: number): number => {
+  return 100 + a;
+};
+
+// console.log(age);
+// console.log(username);
+// console.log(toggle);
+// console.log(empty);
+// console.log(callback);
