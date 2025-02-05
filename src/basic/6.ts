726c697071ea0b24e273d20bbbed22582fb2cc97
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-05 15:08:28 +0200

    Basic tasks 6

---
+++ src/basic/6.ts
@@ -0,0 +1,30 @@
+type User = {
+  name: string;
+  age: number;
+  email: string;
+  address?: {
+    city: string;
+    country: string;
+  };
+};
+
+const mango = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
+};
+
+const poly = {
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+};
+
+// console.log(mango);
+// console.log(poly);
+
+// Створіть інтерфейс User для типізації об'єктів, які містять такі властивості. Зверніть увагу, що адреса є необов'язковою властивістю.
