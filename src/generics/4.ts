4b512948b534df623b5471c8c96c565cef4574b3
Dmtro_Tr <Di4gamegem@gmail.com>
2025-02-06 16:16:37 +0200

    Generics 3,4

--- src/generics/4.ts
+++ src/generics/4.ts
@@ -9,11 +9,21 @@ type User = {
   password: string;
 };
 
-function createOrUpdateUser(initialValues: User) {
+function createOrUpdateUser(initialValues: Partial<User>): User {
   // Оновлення користувача
-}
+  const defaultUser: User = {
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  };
 
+  return { ...defaultUser, ...initialValues };
+}
+// const updatedUser =
 createOrUpdateUser({
   email: "user@mail.com",
   password: "password123",
 });
+
+// console.log(updatedUser);
